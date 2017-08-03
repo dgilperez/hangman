7a3638dd469f66617f0cc0d3d4d901afb3bc987e
David Gil <dgilperez@gmail.com>
2017-08-04 00:06:30 +0200

    :hammer: refactor dictionary functions using function composition

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary do
   def random_word do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random
   end
 
   def word_list do
-    contents = File.read!("assets/lemario-general-del-espanol.txt")
-    String.split(contents, ~r/\n/)
+    "assets/lemario-general-del-espanol.txt"
+    |> File.read!
+    |> String.split(~r/\n/)
   end
 end
