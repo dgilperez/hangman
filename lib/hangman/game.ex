458e5e1f23c0aa5511a077417ae279575480689e
David Gil <dgilperez@gmail.com>
2017-08-12 19:46:20 +0200

    :sparkles: :check_white_mark: hangman game now handles all states from initializing to won or lost

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -3,6 +3,7 @@ defmodule Hangman.Game do
     turns_left: 7,
     game_state: :initializing,
     letters: [],
+    used: MapSet.new,
   )
 
   def new_game do
@@ -15,13 +16,42 @@ defmodule Hangman.Game do
     { game, tally(game) }
   end
 
-  def make_move(game = %{ game_state: state }, guess) do
-    if guess == game.letters |> Enum.join do
-      { Map.put(game, :game_state, :won), tally(game) }
-    else
-      { Map.put(game, :turns_left, game.turns_left - 1), tally(game) }
-    end
+  def make_move(game = %{ turns_left: turns_left }, _guess) when turns_left <= 0 do
+    new_game = Map.put(game, :game_state, :lost)
+    { new_game, tally(game) }
   end
+
+  def make_move(game, guess) do
+    new_game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    { new_game, tally(game) }
+  end
+
+  def accept_move(game, _guess, _already_guest = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  def accept_move(game, guess, _already_guest) do
+    game
+    |> Map.put(:used, MapSet.put(game.used, guess))
+    |> Map.put(:turns_left, game.turns_left - 1)
+    |> verify_guess(Enum.member?(game.letters, guess))
+  end
+
+  def verify_guess(game, _good_guess = true) do
+    new_state = MapSet.new(game.letters)
+                |> MapSet.subset?(game.used)
+                |> maybe_won
+    game
+    |> Map.put(:game_state, new_state)
+  end
+
+  def verify_guess(game, _bad_guess) do
+    game
+    |> Map.put(:game_state, :bad_guess)
+  end
+
+  def maybe_won(_won = true), do: :won
+  def maybe_won(_), do: :good_guess
 
   def tally(_game) do
     123
--- test/game_test.exs
+++ test/game_test.exs
@@ -23,7 +23,7 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [ :won, :lost ] do
       game = Game.new_game |> Map.put(:game_state, state)
-      assert { ^game, _ } = Game.make_move(game, "XXX")
+      assert { ^game, _ } = Game.make_move(game, "x")
     end
   end
 
@@ -31,15 +31,58 @@ defmodule GameTest do
     game = Game.new_game
            |> Map.put(:letters, ["p", "a", "n"])
            |> Map.put(:turns_left, 100)
-    { new_game, _ } = Game.make_move(game, "vino")
+    { new_game, _ } = Game.make_move(game, "v")
 
-    assert new_game.game_state != :won
     assert new_game.turns_left == 99
+  end
+
+  test "guess is recorded if good guess" do
+    game = Game.new_game |> Map.put(:letters, ["p", "a", "n"])
+    { new_game, _ } = Game.make_move(game, "p")
+
+    assert new_game.game_state == :good_guess
+    assert new_game.turns_left == 6
+    assert MapSet.member?(new_game.used, "p")
+  end
+
+  test "guess is recorded if bad guess" do
+    game = Game.new_game |> Map.put(:letters, ["p", "a", "n"])
+    { new_game, _ } = Game.make_move(game, "x")
+
+    assert new_game.game_state == :bad_guess
+    assert new_game.turns_left == 6
+    assert MapSet.member?(new_game.used, "x")
+  end
+
+  test "state is changed to :already_used if guess is repeated" do
+    { game, _ } = Game.new_game
+                  |> Map.put(:letters, ["p", "a", "n"])
+                  |> Game.make_move("p")
+
+    assert MapSet.member?(game.used, "p")
+    assert game.game_state != :already_used
+
+    { new_game, _ } = Game.make_move(game, "p")
+
+    assert MapSet.member?(new_game.used, "p")
+    assert new_game.game_state == :already_used
+  end
+
+  test "game is lost if turns_left is 0 or less" do
+    for turns_left <- [-100, -1, 0] do
+      { game, _ } = Game.new_game
+                    |> Map.put(:turns_left, turns_left)
+                    |> Game.make_move("x")
+      assert game.game_state == :lost
+    end
   end
 
   test "state is changed to :won if guess is correct" do
     game = Game.new_game |> Map.put(:letters, ["p", "a", "n"])
-    { new_game, _ } = Game.make_move(game, "pan")
+    { new_game, _ } = Game.make_move(game, "p")
+    { new_game, _ } = Game.make_move(new_game, "a")
+    { new_game, _ } = Game.make_move(new_game, "n")
+
     assert new_game.game_state == :won
   end
 end
