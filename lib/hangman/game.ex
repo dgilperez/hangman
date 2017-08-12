3a481ec7f6339253e0625001089e8770bef1f3ed
David Gil <dgilperez@gmail.com>
2017-08-12 17:41:38 +0200

    :construction: adds make_move basic functionality for guesses

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -10,4 +10,20 @@ defmodule Hangman.Game do
       letters: Dictionary.random_word |> String.codepoints
     }
   end
+
+  def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
+    { game, tally(game) }
+  end
+
+  def make_move(game = %{ game_state: state }, guess) do
+    if guess == game.letters |> Enum.join do
+      { Map.put(game, :game_state, :won), tally(game) }
+    else
+      { Map.put(game, :turns_left, game.turns_left - 1), tally(game) }
+    end
+  end
+
+  def tally(_game) do
+    123
+  end
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -19,4 +19,27 @@ defmodule GameTest do
     assert game.letters
            |> Enum.map(&(String.match?(&1, ~r/[a-z]/)))
   end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "XXX")
+    end
+  end
+
+  test "turns_left is decremented if guess is incorrect" do
+    game = Game.new_game
+           |> Map.put(:letters, ["p", "a", "n"])
+           |> Map.put(:turns_left, 100)
+    { new_game, _ } = Game.make_move(game, "vino")
+
+    assert new_game.game_state != :won
+    assert new_game.turns_left == 99
+  end
+
+  test "state is changed to :won if guess is correct" do
+    game = Game.new_game |> Map.put(:letters, ["p", "a", "n"])
+    { new_game, _ } = Game.make_move(game, "pan")
+    assert new_game.game_state == :won
+  end
 end
