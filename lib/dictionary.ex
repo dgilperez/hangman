f0f924ea6149ecdb141c7f7be1e4993dadb38d7d
David Gil <dgilperez@gmail.com>
2017-08-15 23:44:43 +0200

    :art: better naming and method order

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,7 +1,7 @@
 defmodule Dictionary do
   alias Dictionary.WordList
 
-  defdelegate start, to: WordList
-  defdelegate start(word_length), to: WordList
+  defdelegate start, to: WordList, as: :word_list
+  defdelegate start(word_length), to: WordList, as: :word_list
   defdelegate random_word(word_list), to: WordList
 end
--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,35 +1,25 @@
 defmodule Dictionary.WordList do
-  def start do
-    load_word_list_from_file()
-  end
-
-  def start(word_length) do
-    start()
-    |> Enum.reject(&(String.length(&1) != word_length))
-    |> Enum.map(&(normalize_word(&1)))
-    |> Enum.uniq
-  end
-
   def random_word(word_list) do
     word_list
     |> Enum.random
     |> normalize_word
   end
 
-  def word_list(word_list) do
-    word_list
-    |> Enum.map(&(normalize_word(&1)))
-    |> Enum.uniq
-  end
-
-  #####
-
-  defp load_word_list_from_file do
+  def word_list do
     "../../assets/lemario-general-del-espanol.txt"
     |> Path.expand(__DIR__)
     |> File.read!
     |> String.split(~r/\n/)
   end
+
+  def word_list(word_length) do
+    word_list()
+    |> Enum.reject(&(String.length(&1) != word_length))
+    |> Enum.map(&(normalize_word(&1)))
+    |> Enum.uniq
+  end
+
+  #####
 
   defp normalize_word(word) do
     word
