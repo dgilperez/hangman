6f40e1648d0d5011b6f6510fa44d808e4b1520db
David Gil <dgilperez@gmail.com>
2017-08-17 20:23:55 +0200

    :hammer: :spark: use an Agent to store word_list

--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,8 +1,18 @@
 defmodule Dictionary.WordList do
-  def random_word(word_list) do
-    word_list
-    |> Enum.random
+  def start_link do
+    Agent.start_link(&word_list/0)
+  end
+
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
     |> normalize_word
+  end
+
+  def word_list(agent, word_length) do
+    Agent.get(agent, &(&1))
+    |> Enum.reject(&(String.length(&1) != word_length))
+    |> Enum.map(&(normalize_word(&1)))
+    |> Enum.uniq
   end
 
   def word_list do
@@ -10,13 +20,6 @@ defmodule Dictionary.WordList do
     |> Path.expand(__DIR__)
     |> File.read!
     |> String.split(~r/\n/)
-  end
-
-  def word_list(word_length) do
-    word_list()
-    |> Enum.reject(&(String.length(&1) != word_length))
-    |> Enum.map(&(normalize_word(&1)))
-    |> Enum.uniq
   end
 
   #####
