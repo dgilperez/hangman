22eb32b10783438cc34ba2a358bc568fe098cbb1
David Gil <dgilperez@gmail.com>
2017-08-19 20:28:59 +0200

    :bug: fix Mover to use new Hangman serverized version

--- lib/text_client/mover.ex
+++ lib/text_client/mover.ex
@@ -2,7 +2,6 @@ defmodule TextClient.Mover do
   alias TextClient.State
 
   def make_move(game = %State{}) do
-    { new_game_service, tally } = Hangman.make_move(game.game_service, game.guess)
-    %State{ game | game_service: new_game_service, tally: tally}
+    %State{ game | tally: Hangman.make_move(game.game_service, game.guess) }
   end
 end
