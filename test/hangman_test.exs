12f4132a8561e7e409c7c3fcfd47e0e7da2bf54e
David Gil <dgilperez@gmail.com>
2017-08-19 20:02:19 +0200

    :sparkles: API handles supervising child games

--- test/hangman_test.exs
+++ test/hangman_test.exs
@@ -2,16 +2,7 @@ defmodule HangmanTest do
   use ExUnit.Case
   doctest Hangman
 
-  test "initializes a new game" do
-    assert %{ game_state: :initializing,
-              turns_left: 7,
-              letters: [_h|_t] } = Hangman.new_game
-  end
-
-  test "makes a move" do
-    game = Hangman.new_game |> Map.put(:letters, ["p", "a", "n"])
-    tally = %{ game_state: :good_guess, turns_left: 7, letters: ["p", "_", "_"], letters_used: ["p"] }
-    assert { game, ^tally } = Hangman.make_move(game, "p")
-    assert Hangman.tally(game) == tally
+  test "initializes a new game, returns a pid" do
+    assert is_pid(Hangman.new_game) == true
   end
 end
