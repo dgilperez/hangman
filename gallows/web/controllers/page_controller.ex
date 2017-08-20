7444224e13d0ae720b55a8fc48dfc71b4d9a5657
David Gil <dgilperez@gmail.com>
2017-08-20 22:00:48 +0200

    playing with assigns

--- gallows/web/controllers/page_controller.ex
+++ gallows/web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Gallows.PageController do
   use Gallows.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    produce = [{"egg", 12}, {"butter spoon", 2}, {"milk glass", 1}, {"missing item", -5}]
+    render conn, "index.html", produce: produce
   end
 end
