ccd18e593eb24d68e8a1a09bdb6aa8e33170a318
David Gil <dgilperez@gmail.com>
2017-08-21 21:09:19 +0200

    :lipstick: complete UI with game state feedback and a few helpers

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,31 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  @responses %{
+    :won           => { :success, "You escaped the hangman! Believe me. It's true" },
+    :lost          => { :danger, "You died. Violently" },
+    :good_guess    => { :success, "That's goddamm right" },
+    :bad_guess     => { :warning, "Meh" },
+    :already_used  => { :info, "Don't repeat yourself, please" },
+  }
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def new_game_button(conn) do
+    button("Play a new game", to: hangman_path(conn, :create_game), class: "btn btn-primary")
+  end
+
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  ###
+
+  defp alert(nil), do: ""
+  defp alert({ class, message }) do
+    content_tag(:div, message, class: "alert alert-#{class}")
+  end
 end
