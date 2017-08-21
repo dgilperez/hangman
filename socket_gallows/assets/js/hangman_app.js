31457bd9925b822c10d7561f51501637901e645e
David Gil <dgilperez@gmail.com>
2017-08-22 01:13:29 +0200

    :heavy_plus_sign: adding Vue as dependency and copy-paste a Vue view to handle all now-fully implemented socket actions

--- socket_gallows/assets/js/hangman_app.js
+++ socket_gallows/assets/js/hangman_app.js
@@ -1,6 +1,66 @@
-import HangmanSocket from "./hangman_socket"
+import HangmanServer from "./hangman_server"
+
+const RESPONSES = {
+    won:          [ "success", "You Won!" ],
+    lost:         [ "danger",  "You Lost!" ],
+    good_guess:   [ "success", "Good guess!" ],
+    bad_guess:    [ "warning", "Bad guess!" ],
+    already_used: [ "info",    "You already guessed that" ],
+    initializing: [ "info",    "Let's Play!" ]
+}
+
+let view = function(hangman) {
+  let app = new Vue({
+    el: "#app",
+    data: {
+      tally: hangman.tally,
+    },
+    computed: {
+      game_over: function() {
+        let state = this.tally.game_state
+        return (state == "won") || (state == "lost")
+      },
+      game_state_message: function() {
+        let state = this.tally.game_state
+        return RESPONSES[state][1]
+      },
+      game_state_class: function() {
+        let state = this.tally.game_state
+        return RESPONSES[state][0]
+      },
+    },
+    methods: {
+      guess: function(letter) {
+        hangman.make_move(letter)
+      },
+      new_game: function() {
+        hangman.new_game()
+      },
+      already_used: function(letter) {
+        return this.tally.used.indexOf(letter) >= 0
+      },
+      correct_guess: function(letter) {
+        return this.already_used(letter) &&
+               this.tally.letters.indexOf(letter) >= 0
+      },
+      turns_gt: function(left) {
+        return this.tally.turns_left > left
+      },
+    }
+  })
+  return app;
+}
 
 window.onload = function() {
-  let hangman = new HangmanSocket()
+  let tally = {
+    turns_left: 7,
+    letters: [],
+    game_state: "initializing",
+    used: [],
+  }
+
+  let hangman = new HangmanServer(tally)
+  let app     = view(hangman)
+
   hangman.connect_to_hangman()
 }
