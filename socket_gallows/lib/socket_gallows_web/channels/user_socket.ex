4122d6fbb1141cd5670761b8c4321e6d384b145e
David Gil <dgilperez@gmail.com>
2017-08-25 00:28:47 +0200

    Use production config from Heroku ENV variables and decrease socket timeout

--- socket_gallows/lib/socket_gallows_web/channels/user_socket.ex
+++ socket_gallows/lib/socket_gallows_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule SocketGallowsWeb.UserSocket do
   channel "hangman:*", SocketGallowsWeb.HangmanChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
